13f78167148b9f9426b9f89781cbc8976e5db39b
Shubham Giri <shubhammgiri3@gmail.com>
2026-01-10 01:31:07 +0530

    setup repository

---
+++ fzf/.fzf.zsh
@@ -0,0 +1,7 @@
+# Auto-completion
+# ---------------
+source "/home/xshubhamg/hypr-dots/fzf/commands/completion.zsh"
+
+# Key bindings
+# ------------
+source "/home/xshubhamg/hypr-dots/fzf/commands/key-bindings.zsh"
